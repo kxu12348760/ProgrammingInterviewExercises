f13c68bc3cb7bd2ef969e4a03af19fdf8bf5c931
Ke Xu <kxu12348760@gmail.com>
2013-03-31 09:19:19 -0700

    README + partial ch2solutions.py

---
+++ ch2Solutions.py
@@ -0,0 +1,349 @@
+#!/usr/bin/python
+
+import unittest
+
+# 2.1: Computing Parity
+def parity1(someInt):
+    print "Calculating parity for: {0}, which is {1}".format(someInt, bin(someInt))
+    parity = 0
+    while someInt > 0:
+        if someInt & 1 == 1:
+            parity ^= 1
+        someInt >>= 1
+    print "parity is {0}\n".format(parity)
+    return parity   
+
+def dropLowestSetBit(someInt):
+    print "Dropping lowest set bit for {0}, which is {1}".format(someInt, bin(someInt))
+    result = someInt & (someInt - 1)
+    print "Result is {0}, which is {1}".format(result, bin(result))
+    return result
+
+def parity2(someInt):
+    print "Calculating parity2 for: {0}, which is {1}".format(someInt, bin(someInt))
+    parity = 0
+    while someInt > 0:
+        parity ^= 1
+        someInt = dropLowestSetBit(someInt)
+    print "parity is {0}\n".format(parity)
+    return parity
+
+'''
+PRECOMPUTED_PARITY_TABLE = (2**16) * [0]
+
+def constructPrecomputedParityTable():
+    for i in range(0, len(PRECOMPUTED_PARITY_TABLE)):
+        PRECOMPUTED_PARITY_TABLE[i] = parity2(i)
+
+def parity3(someInt):
+    print "Calculating parity3 for: {0}, which is {1}".format(someInt, bin(someInt))
+    reverseBits63to48 = PRECOMPUTED_PARITY_TABLE[(someInt >> 48) & 0xFFFF]
+    reverseBits47to32 = PRECOMPUTED_PARITY_TABLE[(someInt >> 32) & 0xFFFF] << 16
+    reverseBits31to16 = PRECOMPUTED_PARITY_TABLE[(someInt >> 16) & 0xFFFF] << 32
+    reverseBits15to0 = PRECOMPUTED_PARITY_TABLE[someInt & 0xFFFF] << 48
+    parity = reverseBits15to0 | reverseBits31to16 | reverseBits47to32 | reverseBits63to48
+    print "parity is {0}".format(parity)
+'''
+
+# 2.2: Swapping bits
+def extractLowestSetBit(someLong, showPrintMsgs=True):
+    if showPrintMsgs:
+        print "Extracting the lowest set bit for {0}, which is {1}".format(someLong, bin(someLong))
+    result = someLong & ~(someLong -1)
+    if showPrintMsgs:
+        print "Result is {0}, which is {1}".format(result, bin(result))
+    return result
+
+def getBitAtIndex(someLong, i, showPrintMsgs=True):
+    if showPrintMsgs:
+        print "Extracting the bit at index {0} for {1}".format(i, someLong)
+    result = 1 if ((someLong & (1 << i)) != 0) else 0
+    if showPrintMsgs:
+        print result
+    return result
+
+def swapBits(someLong, i, j, showPrintMsgs=True):
+    bitAtIndexI = getBitAtIndex(someLong, i, showPrintMsgs)
+    bitAtIndexJ = getBitAtIndex(someLong, j, showPrintMsgs)
+    if bitAtIndexI ^ bitAtIndexJ:
+        if showPrintMsgs:
+            print "swapping bits {0} and {1} for {2}, which is {3} in binary".format(i, j, someLong, bin(someLong))
+        someLong ^= (1 << i)
+        someLong ^= (1 << j)
+    if showPrintMsgs:
+        print "result is {0}, which is {1} in binary\n".format(someLong, bin(someLong))
+    return someLong
+
+# 2.3: Bit reversal
+def reverseBitsRegular(someLong):
+    print "Reversing bits for {0}, which is {1} in binary".format(someLong, bin(someLong))
+    for i in range(0, 32):
+        someLong = swapBits(someLong, i, 63 - i, showPrintMsgs=False)
+    print "result is {0}, which is {1} in binary\n".format(someLong, bin(someLong))
+    return someLong
+
+# 2.4: Closest Integers with the same weight
+def closestIntSameWeight(x):
+    print "Calculating closestIntSameWeight for {0}, which is {1} in binary".format(x, bin(x))
+    for i in range(0, 64):
+        ithBit = getBitAtIndex(x, i, showPrintMsgs = False)
+        jthBit = getBitAtIndex(x, i + 1, showPrintMsgs = False)
+        if ithBit != jthBit:
+            closestInt = swapBits(x, i, i+1, showPrintMsgs = False)
+            print "Calculated closestIntSameWeight for {0} is {1}, or {2} in binary\n".format(x, closestInt, bin(closestInt))
+            return closestInt
+
+# 2.5 Printing power set
+def printPowerSet(myList):
+    print "Printing powerset for {0}".format(myList)
+    myListSet = set(myList)
+    myUniqueList = list(myListSet)
+    setSize = len(myUniqueList)
+    # Get number of subsets = 2^setSize
+    numSubsets = 1 << setSize
+    for i in range(0, numSubsets):
+        subSetList = []
+        # Go through each bit, as the number of bits is equal to setSize, and if set, add to subset
+        for j in range(0, setSize):
+            if getBitAtIndex(i, j, showPrintMsgs = False):
+                subSetList.append(myUniqueList[j])
+        print set(subSetList)
+    print
+
+
+# 2.6 String to int and int to string
+
+def charToInt(someChar, someBase=10):
+    charOrd = ord(someChar)
+    aOrd = ord('A')
+    someInt = 0
+
+    if charOrd >= aOrd:
+        someInt = charOrd - aOrd + 10
+    else:
+        zeroOrd = ord('0')
+        someInt = charOrd - zeroOrd
+
+    if someInt < someBase:
+        return someInt
+    else:
+        raise Exception("Character {0} is not numeric in base {1}".format(someChar, someBase))
+
+def intToChar(someInt, someBase=10):
+    if (someInt >= 0 and someInt < someBase):
+        if (someInt < 10):
+            return chr(ord('0') + someInt)
+        else:
+            return chr(ord('A') + someInt - 10)
+    else:
+        raise Exception("Cannot convert integer {0} to a character".format(someInt))
+
+def stringToInt(someString, someBase=10, showPrintMsgs=True):
+    someInt = 0
+    if len(someString) == 0:
+        raise Exception("Cannot convert empty string to integer")
+    isNegative = (someString[0] == '-')
+    startIndex = 1 if isNegative else 0
+    multiplier = -1 if isNegative else 1
+    if showPrintMsgs:
+        print "Converting {0} to integer".format(someString)
+    for i in range(startIndex, len(someString)):
+        someChar = someString[i]
+        try:
+            charInt = charToInt(someChar, someBase)
+        except Exception, e:
+            print str(e)
+            return
+        someInt = (someBase * someInt + charInt)
+    if showPrintMsgs:
+        print "Converted integer is {0}\n".format(someInt)
+    return multiplier * someInt
+
+def intToString(someInt, someBase=10, showPrintMsgs=True):
+    isNegative = (someInt < 0)
+    someString = ''
+    someInt = abs(someInt)
+    if showPrintMsgs:
+        print "Converting {0} to a string".format(someInt)
+    while (someInt > 0):
+        remainder = someInt % someBase
+        try:
+            remainderChar = intToChar(remainder, someBase)
+        except Exception, e:
+            print str(e)
+            return
+        someString = remainderChar + someString
+        someInt = someInt / someBase
+    someString = '-' + someString if isNegative else someString
+    if showPrintMsgs:
+        print "Resulting string is {0}\n".format(someString)
+    return someString
+
+# 2.7
+# Converting an integer string from one base to another.
+def convertToBase(someIntString, base1, base2, showPrintMsgs=True):
+    if showPrintMsgs:
+        print "Converting {0} in base {1} to base {2}".format(someIntString, base1, base2)
+    integerInBase1 = stringToInt(someIntString, base1, showPrintMsgs=False)
+    stringInBase2 = intToString(integerInBase1, base2, showPrintMsgs=False)
+    if showPrintMsgs:
+        print "Converted string in base {0} is {1}\n".format(base2, stringInBase2)
+    return stringInBase2
+
+# 2.8
+# Spreadsheet column encoding
+def ssDecodeColId(columnIdString, showPrintMsgs=True):
+    if showPrintMsgs:
+        print "Encoding the following column id {0} to an integer".format(columnIdString)
+    colIdInt = 0
+    for someChar in columnIdString:
+        colIdInt = 26 * colIdInt + ord(someChar) - ord('A') + 1
+    if showPrintMsgs:
+        print "The encoded column id integer is {0}\n".format(colIdInt)
+    return colIdInt
+
+# 2.9
+# Elias Gamma Encoding
+def encodeElias(someIntList, showPrintMsgs=True):
+    if showPrintMsgs:
+        print "Encoding the following list {0}".format(someIntList)
+    stringEncoding = ""
+    for someInt in someIntList:
+        someIntString = intToString(someInt, someBase=2, showPrintMsgs=False)
+        stringEncoding = stringEncoding + '0' *( len(someIntString) - 1) + someIntString
+    if showPrintMsgs:
+        print "The Encoding resulted in the following string {0}\n".format(stringEncoding)
+    return stringEncoding
+
+# Elias Gamma Decoding
+def decodeElias(someEncodingString, showPrintMsgs=True):
+    if showPrintMsgs:
+        print "Decoding the following string {0}".format(someEncodingString)
+    someIntList = []
+    numZeroes = 0
+    index = 0
+    stringLen = len(someEncodingString)
+    while index <  stringLen:
+        if someEncodingString[index] is '0':
+            numZeroes += 1
+            index += 1
+        else:
+            newIndex = index + numZeroes + 1
+            subString = someEncodingString[index : newIndex]
+            someInt = stringToInt(subString, someBase=2, showPrintMsgs=False)
+            someIntList.append(someInt)
+            numZeroes = 0
+            index = newIndex
+    if showPrintMsgs:
+        print "The decoding is the following list {0}\n".format(someIntList)
+    return someIntList
+
+# Greatest Common Divisor (Problem 2.10)
+def isEven(someInt):
+    return someInt & 1 == 0
+
+def gcd(a, b, showPrintMsgs=True):
+    isAEven = isEven(a)
+    isBEven = isEven(b)
+    someGcd = 0
+    if a == 0 or b == 0:
+        someGcd = 0
+    elif a == b:
+        someGcd = a
+    elif isAEven and isBEven:
+        someGcd = gcd(a>>1, b>>1, showPrintMsgs=False)<<1
+    elif isAEven:
+        someGcd = gcd(a>>1, b, showPrintMsgs=False)
+    elif isBEven:
+        someGcd = gcd(a, b>>1, showPrintMsgs=False)
+    elif a > b:
+        someGcd = gcd (a-b, b, showPrintMsgs=False)
+    else:
+        someGcd = gcd(a, b-a, showPrintMsgs=False)
+    if showPrintMsgs:
+        print "The gcd of {0} and {1} is {2}".format(a, b, someGcd)
+    return someGcd
+
+class Ch2SolutionsTestCase(unittest.TestCase):
+    # Construct a precomputed table of parities for 16-bit integers
+    # constructPrecomputedParityTable()
+
+    def testParity1(self):
+        # Tests for computing parity using straightforward approach (problem 2.1)
+        self.assertEqual(0, parity1(17))
+        self.assertEqual(1, parity1(28))
+
+    def testParity2(self):
+        # Tests for computing parity using approach where each lowest set bit is dropped (problem 2.1)
+        self.assertEqual(0, parity2(17))
+        self.assertEqual(1, parity2(28))
+
+    def testParity1AndParity2ReturnsSameResults(self):
+        self.assertEqual(parity1(17), parity2(17))
+        self.assertEqual(parity1(28), parity2(28))
+
+    def testSwapBits(self):
+        # Tests for swapping bits (problem 2.2)
+        self.assertEqual(int(0b10001111001000), swapBits(0b10001110001001, 0, 6))
+        self.assertEqual(int(0b00010), swapBits(0b10000, 4, 1))
+
+    def testReverseBitsRegular(self):
+        # Tests for bit reversal (problem 2.3)
+        self.assertEqual(int(0b0111010010000000000000000000000000000000000000000000000000000000),
+            reverseBitsRegular(0b100101110))
+        self.assertEqual(int(0b1100100000000000000000000000000000000000000000000000000000000000),
+            reverseBitsRegular(0b00010011))
+
+    def testClosestIntSameWeight(self):
+        # Tests for closest int same weight (problem 2.4)
+        self.assertEqual(101, closestIntSameWeight(99))
+        self.assertEqual(234, closestIntSameWeight(236))
+
+    def testPrintPowerSet(self):
+        # Tests for printing power set (problem 2.5)
+        printPowerSet([1, 2, 3, 4])
+        printPowerSet([1, 1, 2, 3])
+
+    def testStringToInt(self):
+        # Tests for string to integer conversion (problem 2.6)
+        self.assertEqual(12335, stringToInt("12335"))
+        stringToInt("asdf")
+        self.assertEqual(-8981, stringToInt("-8981"))
+
+    def testIntToString(self):
+        # Tests for integer to string conversion (problem 2.6)
+        self.assertEqual("-8928", intToString(-8928))
+        self.assertEqual("819211", intToString(819211))
+
+    def testConvertToBase(self):
+        # Tests for converting an integer string between 2 bases (problem 2.7)
+        self.assertEquals("11259375", convertToBase("ABCDEF", 16, 10))
+        self.assertEqual("1E44D", convertToBase("123981", 10, 16))
+        self.assertEqual("1010", convertToBase("A", 16, 2))
+        self.assertEqual("27", convertToBase("123", 4, 10))
+
+    def testSSDecodeColId(self):
+        # Tests for spreadsheet column encoding (problem 2.8)
+        self.assertEqual(1, ssDecodeColId("A"))
+        self.assertEqual(26, ssDecodeColId("Z"))
+        self.assertEqual(27, ssDecodeColId("AA"))
+        self.assertEqual(703, ssDecodeColId("AAA"))
+
+    def testEncodeElias(self):
+        # Tests for Elias Gamma Encoding (problem 2.9)
+        self.assertEqual("00011010000001000000000010100", encodeElias([13, 64, 20]))
+        self.assertEqual([13, 64, 20], decodeElias("00011010000001000000000010100"))
+
+    def testGCD(self):
+        # Tests for GCD (problem 2.10)
+        self.assertEqual(1, gcd(17, 10))
+        self.assertEqual(38, gcd(38, 190))
+        self.assertEqual(1, gcd(5, 19))
+        self.assertEqual(0, gcd(0, 10))
+        self.assertEqual(10, gcd(10, 20))
+        self.assertEqual(6, gcd(12, 30))
+
+if __name__ == '__main__':
+    unittest.main()
+    
+
